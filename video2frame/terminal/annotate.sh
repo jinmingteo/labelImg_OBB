35821dba1193a1854ae8cad9736bb6d8983439d6
dh <jinmingteo95@gmail.com>
2021-01-19 15:09:19 +0800

    [predefined_classes] improve terminal and predefined classes

--- video2frame/terminal/annotate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xhost +local:docker && docker run --rm -e "DISPLAY=${DISPLAY}" -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" --user "$(id -u):$(id -g)" -v $offline_annotation_repo:/workspace/videos offline_annotator
